e16f52bcdd60ea82ce3c6d7de994dd4566385ab7
treinacloud <52660631+treinacloud@users.noreply.github.com>
2019-08-02 13:06:41 +0100

    Update agent.js

--- src/agent.js
+++ src/agent.js
@@ -3,7 +3,7 @@ import _superagent from 'superagent';
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const API_ROOT = 'https://backend.cloudcamp.cf';
+const API_ROOT = 'https://backend.cloudcamp.cf/api';
 
 const encode = encodeURIComponent;
 const responseBody = res => res.body;
